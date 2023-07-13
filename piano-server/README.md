253bf7e900650521633d64796e8b6bb30c8d2c09
Jordan Pitlor <jordan@pitlor.dev>
2023-07-12 23:31:30 -0400

    finally set up ws

---
+++ piano-server/README.md
@@ -0,0 +1,1 @@
+Dependencies: https://github.com/Theldus/wsServer
--- piano-server/main.c
+++ piano-server/main.c
@@ -1,7 +1,41 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <ws.h>
+#include <string.h>
+#include <stdio.h>
+
+ws_cli_conn_t* pi;
+
+void onopen(ws_cli_conn_t *client) {
+    pi = client;
+}
+
+void play_note(char* note) {
+    printf("Playing note %s\n", note);
+}
+
+void onmessage(ws_cli_conn_t *client, const unsigned char *msg, uint64_t size, int type) {
+    char mutable_message[size];
+    strcpy(mutable_message, (const char*)msg);
+
+    char* token = strtok(mutable_message, ":");
+    while (token != NULL) {
+        play_note(token);
+        token = strtok(NULL, ":");
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+    struct ws_events evs;
+    evs.onopen = &onopen;
+    evs.onmessage = &onmessage;
+
+    ws_socket(&evs, 8080, 1, 0);
+
+    while (1) {
+        sleep(3);
+    }
+
+    // Listen for key presses and send to the browser
+
     return 0;
 }
